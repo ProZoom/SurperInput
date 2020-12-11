e1911bdf53e083267b83ce91ff3c63415e964ba9
leo <liyang.ok@outlook.com>
2020-12-11 17:34:23 +0800

    Initial commit

---
+++ app/src/main/java/com/surperinput/sample/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.surperinput.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //super_input
+
+        input.attachParent(this,supportFragmentManager)
+
+
+    }
+}
